7cae3018714aeb52f34ca758e2bc455124045abd
sanjiblamichhane <sanjib@jibmac.lan>
2021-01-12 15:24:48 -0500

    Heroku config

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic', # add this above staticfiles --> for heroku deployment
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig', #blog app
     'accounts.apps.AccountsConfig', # signup app
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhitenoiseMiddleware', #added for heroku deployment of static files
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -119,8 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #added for heroku deployment
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'home' # the user will be redirected to the 'home ' template which is our home page
 LOGOUT_REDIRECT_URL = 'home' # the user will be redirected to the 'home ' template which is our home page
